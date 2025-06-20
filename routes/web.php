e6163ada39c17305ac390a2c5bb1ad298fecf2ec
Ahmedf <ahmedfaisalzayed288@gmail.com>
2025-06-20 09:49:54 +0300

    add option of controller group to routes

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,27 @@
 use App\Http\Controllers\SystemController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/','index');
+Route::controller(SystemController::class)->group(function (){
+    Route::view('/','index');
 
-Route::get('/show',[SystemController::class,'login'])->name('login');
-Route::get('/show/student',[SystemController::class,'show_student'])->name('show_student');
-Route::get('/show/content/{class}/{subject}',[SystemController::class,'show_student_content'])->name('show_student_content');
-Route::get('/show/content/{class}/{subject}/lessons',[SystemController::class,'show_student_lesson'])->name('show_student_lesson');
-Route::get('/show/content/{class}/{subject}/homeworks',[SystemController::class,'show_student_homework'])->name('show_student_homework');
-Route::get('/show/quiz/{class}/{subject}',[SystemController::class,'show_student_quizzes'])->name('show_student_quizzes');
-Route::get('/show/{class}/{subject}/content/quiz',[SystemController::class,'show_content_quiz'])->name('show_content_quiz');
-Route::post('/store/{class}/{subject}/student/answers',[SystemController::class,'store_student_answers'])->name('store_student_answers');
-Route::get('/log_out',[SystemController::class,'log_out_student'])->name('log_out_student');
+    // Routes of Student
 
-Route::get('/show/teacher',[SystemController::class,'show_teacher'])->name('show_teacher');
-Route::post('/store/teacher/{teacher}',[SystemController::class,'store_teacher'])->name('store_teacher');
-Route::get('/show/teacher/{teacher}/lessons',[SystemController::class,'show_teacher_lessons'])->name('show_teacher_lessons');
-Route::get('/show/teacher/{teacher}/homeworks',[SystemController::class,'show_teacher_homeworks'])->name('show_teacher_homeworks');
-Route::get('/create/teacher/{teacher}/quiz',[SystemController::class,'create_teacher_quiz'])->name('create_teacher_quiz');
+    Route::get('/show','login')->name('login');
+    Route::get('/show/student','show_student')->name('show_student');
+    Route::get('/show/content/{class}/{subject}','show_student_content')->name('show_student_content');
+    Route::get('/show/content/{class}/{subject}/lessons','show_student_lesson')->name('show_student_lesson');
+    Route::get('/show/content/{class}/{subject}/homeworks','show_student_homework')->name('show_student_homework');
+    Route::get('/show/quiz/{class}/{subject}','show_student_quizzes')->name('show_student_quizzes');
+    Route::get('/show/{class}/{subject}/content/quiz','show_content_quiz')->name('show_content_quiz');
+    Route::post('/store/{class}/{subject}/student/answers','store_student_answers')->name('store_student_answers');
+    Route::get('/log_out','log_out_student')->name('log_out_student');
+
+    //Routes of Teacher
+
+    Route::get('/show/teacher','show_teacher')->name('show_teacher');
+    Route::post('/store/teacher/{teacher}','store_teacher')->name('store_teacher');
+    Route::get('/show/teacher/{teacher}/lessons','show_teacher_lessons')->name('show_teacher_lessons');
+    Route::get('/show/teacher/{teacher}/homeworks','show_teacher_homeworks')->name('show_teacher_homeworks');
+    Route::get('/create/teacher/{teacher}/quiz','create_teacher_quiz')->name('create_teacher_quiz');
+
+});
