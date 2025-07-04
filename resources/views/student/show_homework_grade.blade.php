26322e23cdcdc38ddb0d7f378e48f73e0d318e37
Ahmedf <ahmedfaisalzayed288@gmail.com>
2025-07-04 22:28:18 +0300

    add action if student didn't upload solution for homework

--- resources/views/student/show_homework_grade.blade.php
+++ resources/views/student/show_homework_grade.blade.php
@@ -7,6 +7,9 @@
 
     <div class="bg-white shadow-lg rounded-lg w-full max-w-md p-6 border-t-4 border-blue-600">
         <h2 class="text-xl font-bold text-gray-800 mb-4 text-center">تقييمك في الواجب</h2>
+        @if (is_null($student_homework_solution))
+                <p class="text-red-500 text-bold">* لا يوجد حل لك لهذا الواجب</p>
+        @else
             @if (is_null($student_homework_grade))
                 <p class="text-red-500 text-bold">لم يتم تقييمك بعد *</p>
             @else
@@ -27,6 +30,7 @@
                     </div>
                 </div>
             @endif
+        @endif
                 <div class="mt-6 text-center">
                     <a href="{{route('show_student_homework',[$class,$subject])}}" class="inline-block bg-blue-600 hover:bg-blue-700 text-white text-sm px-4 py-2 rounded">
                         العودة إلى قائمة الواجبات
